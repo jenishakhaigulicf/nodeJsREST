d07eecebff1a7129d92d7540f520b376342549a5
Jenisha <jenisha@192.168.123.2>
2024-10-01 15:17:42 +0545

    381: getting data from the db

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,19 +3,16 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "image/duck.jpg",
-        creator: {
-          name: "Jenisha",
-        },
-        createdAt: new Date(),
-      },
-    ],
+  Post.findAll()
+    .then((posts) => {
+      res.status(200).json({ message: "Fetched posts successfully", posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      console.log(err);
+      next(err);
     });
 };
 
@@ -54,19 +51,14 @@ exports.createPosts = (req, res, next) => {
 };
 
 exports.getPost = (req, res, next) => {
-  // get id
   const postId = req.params.postId;
-  // find by id from the db
   Post.findByPk(postId)
     .then((post) => {
-      // if not post found then throw error
       if (!post) {
         const error = new Error("Could not find the post.");
         error.statusCode = 404;
         throw error;
-        // goes to catch block
       }
-      // if no error then send response with status code
       res.status(200).json({ message: "Post fetched successfully", post });
     })
     .catch((err) => {
