0c305c053cdc26f599cf77fddab525d1517eb81d
Jenisha <jenisha@192.168.123.2>
2024-10-01 15:18:43 +0545

    381: fetching a single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -51,14 +51,19 @@ exports.createPosts = (req, res, next) => {
 };
 
 exports.getPost = (req, res, next) => {
+  // get id
   const postId = req.params.postId;
+  // find by id from the db
   Post.findByPk(postId)
     .then((post) => {
+      // if not post found then throw error
       if (!post) {
         const error = new Error("Could not find the post.");
         error.statusCode = 404;
         throw error;
+        // goes to catch block
       }
+      // if no error then send response with status code
       res.status(200).json({ message: "Post fetched successfully", post });
     })
     .catch((err) => {
